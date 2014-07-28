2cbba49fa4c9bbbe28f50785e8292d4f97162cd2
mesoluke <mesoluke@hotmail.com>
2014-07-28 01:27:50 -0500

    Update Credentials.ps1

--- Credentials.ps1
+++ Credentials.ps1
@@ -1,11 +1,15 @@
 #TODO: Store creds in a reg key
 #These functions can be used with email scripts (for instance) in order to store passwords and login info locally without exposing and information.
-#
+#NOTES:
+#$Credentials.GetNetworkCredential().Password will reveal the plain text password when $Credentials.
 #Sources:
 #http://stackoverflow.com/questions/12460950/how-to-pass-credentials-to-the-send-mailmessage-command-for-sending-emails
 #https://www.interworks.com/blogs/trhymer/2013/07/08/powershell-how-encrypt-and-store-credentials-securely-use-automation-script
 #http://technet.microsoft.com/en-us/library/hh849814.aspx
 #http://technet.microsoft.com/en-us/library/hh849815.aspx
+#http://technet.microsoft.com/en-us/library/hh849818.aspx
+#http://social.technet.microsoft.com/wiki/contents/articles/4546.working-with-passwords-secure-strings-and-credentials-in-windows-powershell.aspx
+
 
 
 Function StoreCredential($pathToCredFile)
