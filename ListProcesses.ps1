56f64bce1dfaebc1a9b9a6d5ecaa749706c0f17f
mesoluke <mesoluke@hotmail.com>
2014-08-26 16:30:53 -0500

    Update ListProcesses.ps1

--- ListProcesses.ps1
+++ ListProcesses.ps1
@@ -5,25 +5,28 @@
 
 $process = "wscript.exe", "cscript.exe", "obclnt32.exe", "powershell.exe"
 $procJobs = (Get-WmiObject win32_process | where{If($process -match $_.ProcessName){$TRUE}}) 
-$myOutput = ""
-foreach ($proc in $procJobs) {
-  if ((($proc.ProcessName -eq "obclnt.exe") -and ($proc.commandline.ToLower().Contains("ODBC".ToLower()))) -or ($proc.ProcessName -ne "obclnt.exe")) {
-    $myOutput = "USER         : "
-    $myOutput += $proc.getowner().domain
-    $myOutput += "\"
-    $myOutput += $proc.getowner().user
-    $myOutput += "`r`n"
-    $myOutput += "COMMAND LINE : "
-    $myOutput += $proc.commandline
-    $myOutput += "`r`n"
-    $myOutput += "CREATION DATE: "
-    $myOutput += ([WMI]'').ConvertToDateTime($proc.creationdate)
-    $myOutput += "`r`n"
-    $myOutput += "PROCESS ID   : "
-    $myOutput += $proc.ProcessId
-    $myOutput += "`r`n"
-    write-host $myOutput
-    Out-File ([Environment]::GetFolderPath("Desktop") + '\Processes.txt') -inputobject $myOutput -Append -Encoding "Default"
+$numJobs = ($procJobs | Measure-Object).count
+if ($numJobs -gt 0) {
+  foreach ($proc in $procJobs) {
+    if ((($proc.ProcessName -eq "obclnt.exe") -and ($proc.commandline.ToLower().Contains("ODBC".ToLower()))) -or ($proc.ProcessName -ne "obclnt.exe")) {
+      $myOutput = "USER         : "
+      $myOutput += $proc.getowner().domain
+      $myOutput += "\"
+      $myOutput += $proc.getowner().user
+      $myOutput += "`r`n"
+      $myOutput += "COMMAND LINE : "
+      $myOutput += $proc.commandline
+      $myOutput += "`r`n"
+      $myOutput += "CREATION DATE: "
+      $myOutput += ([WMI]'').ConvertToDateTime($proc.creationdate)
+      $myOutput += "`r`n"
+      $myOutput += "PROCESS ID   : "
+      $myOutput += $proc.ProcessId
+      $myOutput += "`r`n"
+      write-host $myOutput
+      Out-File ([Environment]::GetFolderPath("Desktop") + '\Processes.txt') -inputobject $myOutput -Append -Encoding "Default"
+    }
   }
 }
+Write-Host "$numJobs jobs found..." 
 $out = Read-Host 'Press Enter...'
