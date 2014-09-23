84f840233ecfc624a489c3959a29194c57a90852
mesoluke <mesoluke@hotmail.com>
2014-09-23 15:30:13 -0500

    Update SIDs.ps1

--- SIDs.ps1
+++ SIDs.ps1
@@ -9,3 +9,31 @@ $users = $computer.psbase.Children | Where-Object { $_.psbase.schemaclassname -e
 foreach ($user in $users) {
    Write-Host ($user.Name).PadRight(30) ($user.Description).PadRight(100) (((New-Object System.Security.Principal.NTAccount($user.Name)).Translate([System.Security.Principal.SecurityIdentifier])).Value)
 }
+
+<#
+
+#Domain User to SID
+
+#This will give you a Domain User's SID
+
+$objUser = New-Object System.Security.Principal.NTAccount("DOMAIN_NAME", "USER_NAME") 
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier]) 
+$strSID.Value
+  
+
+#SID to Domain User
+
+#This will allow you to enter a SID and find the Domain User
+
+$objSID = New-Object System.Security.Principal.SecurityIdentifier ` 
+ ("ENTER-SID-HERE") 
+$objUser = $objSID.Translate( [System.Security.Principal.NTAccount]) 
+$objUser.Value 
+  
+#LOCAL USER to SID
+
+$objUser = New-Object System.Security.Principal.NTAccount("LOCAL_USER_NAME") 
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier]) 
+$strSID.Value
+
+#>
